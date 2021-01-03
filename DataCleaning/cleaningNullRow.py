f780415d2f527d0ac77b07207a81479dfe9d4ecb
yuhQ99 <huy107199900@gmail.com>
2021-01-03 20:29:29 +0700

    fix/datascraping

--- DataCleaning/cleaningNullRow.py
+++ DataCleaning/cleaningNullRow.py
@@ -1,23 +1,23 @@
 import pandas as pd
 
-df = pd.read_csv("../DataScraping/forwards.csv")
+df = pd.read_csv("../data/forwards.csv")
 
 df_filtered = df[df['Appearance'] > 0]
 
 df_filtered.to_csv('../data/forwards.csv', index=False, encoding='utf-8')
 
-df = pd.read_csv("../DataScraping/midfielders.csv")
+df = pd.read_csv("../data/midfielders.csv")
 df_filtered = df[df['Appearance'] > 0]
 
 df_filtered.to_csv('../data/midfielders.csv', index=False, encoding='utf-8')
 
-df = pd.read_csv("../DataScraping/defenders.csv")
+df = pd.read_csv("../data/defenders.csv")
 
 df_filtered = df[df['Appearance'] > 0]
 
 df_filtered.to_csv('..data/defenders.csv', index=False, encoding='utf-8')
 
-df = pd.read_csv("../DataScraping/goalkeepers.csv")
+df = pd.read_csv("../data/goalkeepers.csv")
 
 df_filtered = df[df['Appearance'] > 0]
 
